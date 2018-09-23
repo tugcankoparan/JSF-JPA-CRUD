bde4dcea3ac3800f5bac7133a9bf1753bb921bf2
Tuğcan Koparan <tugcankoparan10@gmail.com>
2018-09-23 18:18:33 +0300

    Update SchoolBean.java

--- tugcankoparan/src/com/jsf/jpa/crud/SchoolBean.java
+++ tugcankoparan/src/com/jsf/jpa/crud/SchoolBean.java
@@ -36,24 +36,19 @@ public class SchoolBean {
 		this.editSchoolId = editSchoolId;
 	}
 
-	// Method To Fetch The Existing School List From The Database
 	public List<SchoolBean> schoolListFromDb() {
 		return DatabaseOperations.getAllSchoolDetails();		
-	}
-	// Method To Add New School To The Database
+	} 
+	
 	public String addNewSchool(SchoolBean schoolBean) {
 		return DatabaseOperations.createNewSchool(schoolBean.getName());		
 	}
-	// Method To Delete The School Details From The Database
 	public String deleteSchoolById(int schoolId) {		
 		return DatabaseOperations.deleteSchoolDetails(schoolId);		
 	}
-	// Method To Navigate User To The Edit Details Page And Passing Selecting School Id Variable As A Hidden Value
-	public String editSchoolDetailsById() {
 		editSchoolId = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("selectedSchoolId");		
 		return "schoolEdit.xhtml";
 	}
-	// Method To Update The School Details In The Database
 	public String updateSchoolDetails(SchoolBean schoolBean) {
 		return DatabaseOperations.updateSchoolDetails(Integer.parseInt(schoolBean.getEditSchoolId()), schoolBean.getName());		
 	}
